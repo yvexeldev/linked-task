31bdc434e0d82c05d6632634a9151abfc1746760
A'zamjon Abdusalomov <a.abdusalomov@agrobank.uz>
2025-03-24 10:50:59 +0500

    init

--- src/run.ts
+++ src/run.ts
@@ -3,32 +3,8 @@ import ITask from './Task';
 
 export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
     maxThreads = Math.max(0, maxThreads);
-
-    executor.start();
-
-    try {
-        const taskChains = new Map<number, Promise<void>>();
-        const activeTasks = new Set<Promise<void>>();
-
-        for await (const task of queue) {
-            const { targetId } = task;
-
-            if (maxThreads > 0 && activeTasks.size >= maxThreads) {
-                await Promise.race(activeTasks);
-            }
-
-            const previousTask = taskChains.get(targetId) || Promise.resolve();
-            const currentTask = previousTask.then(() => executor.executeTask(task));
-
-            taskChains.set(targetId, currentTask);
-
-            const wrappedTask = currentTask.finally(() => activeTasks.delete(wrappedTask));
-
-            activeTasks.add(wrappedTask);
-        }
-
-        await Promise.all(activeTasks);
-    } finally {
-        executor.stop();
-    }
+    /**
+     * Код надо писать сюда
+     * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
+     */
 }
